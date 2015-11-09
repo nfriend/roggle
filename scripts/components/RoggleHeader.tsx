daf49abaefc8faa54e34aa37014f086bd005dc52
Nathan Friend <hello@nathanfriend.io>
2015-11-08 23:44:21 -0600

    added media links

--- scripts/components/RoggleHeader.tsx
+++ scripts/components/RoggleHeader.tsx
@@ -32,7 +32,27 @@ module Roggle {
 						<div>
 							<div className="share-link-title">Invite people to this gameroom by sharing this URL: </div>
 							<div className="cleared"></div>
-							<div className="share-link">{this.state.currentUrl}&nbsp;&nbsp;</div>
+							<div className="share-link">{this.state.currentUrl}</div>
+						</div>
+						<div className="media-link-container">
+							<a className="media-link email-link" href="mailto:roggle@nathanfriend.io?subject=Roggle">
+								<i className="fa fa-envelope-square" />
+							</a>
+							<a className="media-link linkedin-link" href="http://www.linkedin.com/in/nfriend">
+								<i className="fa fa-linkedin-square" />
+							</a>
+							<a className="media-link googleplus-link" href="https://plus.google.com/100251709863074180329/posts">
+								<i className="fa fa-google-plus-square" />
+							</a>
+							<a className="media-link facebook-link" href="https://twitter.com/NathanAFriend">
+								<i className="fa fa-twitter-square" />
+							</a>
+							<a className="media-link facebook-link" href="https://www.facebook.com/nathan.friend">
+								<i className="fa fa-facebook-square" />
+							</a>
+							<a className="media-link github-link" href="https://github.com/nfriend/roggle">
+								<i className="fa fa-github-square" />
+							</a>
 						</div>
 					</div>
 					<div className="cleared"></div>
